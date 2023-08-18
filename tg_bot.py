6da5ab4f31b6b430db4916cccdc30fc94abaa3c7
Art Gilyazov <art.gilyazov@mail.ru>
2023-08-18 08:12:42 +0300

    publish photo to Telegram channel

--- tg_bot.py
+++ tg_bot.py
@@ -8,3 +8,4 @@ load_dotenv()
 bot = telegram.Bot(token=os.environ["TG_BOT_TOKEN"])
 
 bot.send_message(text='Hello!', chat_id='@astronomy_tatarstan')
+bot.send_document(chat_id='@astronomy_tatarstan', document=open('images/spacex0.jpg', 'rb'))
